b1cf78d3c05e6b10033930348e9e890a2a11358d
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-01-23 23:07:04 +0100

    the clerk is initiated

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import NavBar from "./components/NavBar";
-// import Home from "./pages/Home";
-// import Movies from "./pages/Movies";
-// import MovieDetails from "./pages/MovieDetails";
-// import SeatLayout from "./pages/SeatLayout";
-// import MyBookings from "./pages/MyBookings";
-// import Favorite from "./pages/Favorite";
-// import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favorite from "./pages/Favorite";
+import Footer from "./components/Footer";
 
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith("/admin");
@@ -18,14 +18,14 @@ const App = () => {
       <Toaster />
       {!isAdminRoute && <NavBar />}
       <Routes>
-        {/* <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:id" element={<MovieDetails />} />
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/favorite" element={<Favorite />} /> */}
+        <Route path="/favorite" element={<Favorite />} />
       </Routes>
-      {/* {!isAdminRoute && <Footer />} */}
+      {!isAdminRoute && <Footer />}
     </>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
-
-import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
-
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from "lucide-react";
+
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
 
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
@@ -14,9 +19,9 @@ const NavBar = () => {
           className="
     relative inline-block text-3xl
     after:absolute after:left-0 after:-bottom-1
-    after:h-[2px] after:w-full
+    after:h-0.5 after:w-full
     after:origin-left after:scale-x-0
-    after:bg-gradient-to-r after:from-primary after:to-white
+    after:bg-linear-to-r after:from-primary after:to-white
     after:transition-transform after:duration-300
     hover:after:scale-x-100
   "
@@ -26,8 +31,8 @@ const NavBar = () => {
       </Link>
       <div
         className={`max-md:absolute max-md:top-0 max-md:right-0 max-md:font-medium max-md:text-lg z-50 flex 
-        flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen 
-        min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 
+        flex-col md:flex-row items-center max-md:justify-center gap-8 md:px-8 py-3 max-md:h-screen 
+        md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 
         overflow-hidden transition-[width] duration-300 ${isOpen ? "max-md:w-full" : "max-md:w-0"}`}
       >
         <XIcon
@@ -77,9 +82,24 @@ const NavBar = () => {
       </div>
       <div className="flex items-center gap-8">
         <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-        <button className="px-4 y-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Log In
-        </button>
+        {!user ? (
+          <button
+            onClick={openSignIn}
+            className="px-4 y-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+          >
+            Log In
+          </button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Bookings"
+                labelIcon={<TicketPlus width={15} />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
       </div>
       <MenuIcon
         className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer"
