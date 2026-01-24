a7640db86440bb7f86b7bf3c897584a35c200f5d
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-01-24 15:05:15 +0100

    hmm

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
   return (
     <div
       className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 
-    bg-[url("assets\backgroundImage.png")] bg-cover bg-center h-screen'
+    bg-[url("assets/backgroundImage.png")] bg-cover bg-center h-screen'
     >
       <img src={assets.marvelLogo} alt="header bg" className="max-h-11 mt-20" />
       <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110 ">
