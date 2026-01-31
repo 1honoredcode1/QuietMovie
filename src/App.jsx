056d38f5fa87b25a5f4769f57f9faf6769317a4f
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-01-31 20:56:06 +0100

    watchout, the admins are here

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import { Route, Routes, useLocation } from "react-router-dom";
 
 import { Toaster } from "react-hot-toast";
 
+import { SignIn } from "@clerk/clerk-react";
+
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
 import SeatLayout from "./pages/SeatLayout";
 import MyBookings from "./pages/MyBookings";
 import Favorite from "./pages/Favorite";
-import Footer from "./components/Footer";
 import Layout from "./pages/admin/Layout";
 import Dashboard from "./pages/admin/Dashboard";
 import AddShows from "./pages/admin/AddShows";
 import ListShows from "./pages/admin/ListShows";
 import ListBookings from "./pages/admin/ListBookings";
 
+import { useAppContext } from "./context/AppContext";
+
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith("/admin");
+  const { user } = useAppContext();
   return (
     <>
       <Toaster />
@@ -29,7 +35,19 @@ const App = () => {
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
         <Route path="/favorite" element={<Favorite />} />
-        <Route path="/admin/*" element={<Layout />}>
+
+        <Route
+          path="/admin/*"
+          element={
+            user ? (
+              <Layout />
+            ) : (
+              <div className="min-h-screen flex justify-center items-center">
+                <SignIn fallbackRedirectUrl={"/admin"} />
+              </div>
+            )
+          }
+        >
           <Route index element={<Dashboard />} />
           <Route path="add-shows" index element={<AddShows />} />
           <Route path="list-shows" index element={<ListShows />} />
